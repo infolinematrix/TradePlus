5c7f3e57c968bdd8d50c9e4bd71a7091b5cd65ae
Subha <infolinematrix@gmail.com>
2019-08-10 12:53:23 +0530

    Subha

--- app/Helpers/Application.php
+++ app/Helpers/Application.php
@@ -6,21 +6,46 @@
  * Time: 12:42 PM
  */
 
-
-
-use ReactorCMS\Entities\NodeMeta;
-use Reactor\Documents\Media\Media;
-
-use ReactorCMS\Entities\Node;
 use ReactorCMS\Entities\Settings;
 //use App\Support\Database\CacheQueryBuilder;
 
+//Time ellaps
 
+if (!function_exists('time_elapsed_string')) {
+    function time_elapsed_string($datetime, $full = false)
+    {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
 
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
 
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        if (!$full) {
+            $string = array_slice($string, 0, 1);
+        }
+
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+    }
+}
 /*Get Mail Confriguration*/
-
-
 
 /*Get Settings*/
 
@@ -43,5 +68,3 @@ if (!function_exists('getSettings')) {
 
     }
 }
-
-
